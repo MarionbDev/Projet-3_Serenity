f1755077915d0eace0e7a7cd45ad7fbec4012bed
MarionbDev <marionbaston84@gmail.com>
2023-06-08 12:26:07 +0200

    creation routesDoctor

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,7 +4,7 @@ import CreateDoctor from "./components/CreateDoctor";
 import "./App.css";
 import Home from "./pages/Home";
 import Connexion from "./pages/Connexion";
-import PrepaPatientMobile from "./pages/PrepaPatientMobile";
+// import PrepaPatientMobile from "./pages/PrepaPatientMobile";
 import Test from "./components/Test";
 
 function App() {
@@ -15,10 +15,7 @@ function App() {
 
         <Route path="/patient" element={<Connexion utilisateur="patient" />} />
         <Route path="/patient/:id" element={<Test utilisateur="patient" />} />
-        <Route
-          path="/patient/preparation-patient"
-          element={<PrepaPatientMobile utilisateur="patient" />}
-        />
+
         <Route path="/doctor" element={<Connexion utilisateur="doctor" />} />
         {/* <Route path="/doctor/:id" element={<Test utilisateur="doctor" />} /> */}
         <Route path="/create-doctor" element={<CreateDoctor />} />
