9e7d6957b631f4e5382c45855fb7e41592113ecb
MarionbDev <marionbaston84@gmail.com>
2023-06-08 10:30:33 +0200

    context

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,18 +1,25 @@
 import { Routes, Route } from "react-router-dom";
+import { UserContextProvider } from "./contexts/UserContext";
 import CreateDoctor from "./components/CreateDoctor";
-
 import "./App.css";
 import Home from "./pages/Home";
 import Connexion from "./pages/Connexion";
+import Test from "./components/Test";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/patient" element={<Connexion utilisateur="patients" />} />
-      <Route path="/doctor" element={<Connexion utilisateur="doctors" />} />
-      <Route path="/create-doctor" element={<CreateDoctor />} />
-    </Routes>
+    <UserContextProvider>
+      <Routes>
+        <Route path="/" element={<Home />} />
+
+        <Route path="/patient" element={<Connexion utilisateur="patient" />} />
+        <Route path="/patient/:id" element={<Test utilisateur="patient" />} />
+
+        <Route path="/doctor" element={<Connexion utilisateur="doctor" />} />
+        <Route path="/doctor/:id" element={<Test utilisateur="doctor" />} />
+        <Route path="/create-doctor" element={<CreateDoctor />} />
+      </Routes>
+    </UserContextProvider>
   );
 }
 
