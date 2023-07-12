5fa1c3f9380f5e7cb490ec2d2b0d9425ca0e838b
MarionbDev <marionbaston84@gmail.com>
2023-07-12 16:52:44 +0200

    mise a jour formulaire sans css responsive et list doctor

--- frontend/src/componentsDoctor/CreateDoctor.jsx
+++ frontend/src/componentsDoctor/CreateDoctor.jsx
@@ -1,7 +1,6 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import SideBarDoctor from "./SideBarDoctor";
-import edit from "../assets/logo/logoDoctor/edit.png";
 import HeaderDoctor from "./HeaderDoctor";
 
 export default function CreateDoctor() {
@@ -12,15 +11,15 @@ export default function CreateDoctor() {
   const [mail, setMail] = useState("");
   const [password, setPassword] = useState("");
   // const [image, setImage] = useState("");
-  // const [language, setLanguage] = useState("");
-  // const [bio, setBio] = useState("");
-  // const [certificate, setCertificate] = useState("");
+  const [language, setLanguage] = useState("");
+  const [bio, setBio] = useState("");
+  const [certificate, setCertificate] = useState("");
   const [role, setRole] = useState("");
-  // const [otherFormation, setOtherFormation] = useState("");
-  // const [experience, setExperience] = useState("");
-  // const [partnership, setPartnership] = useState("");
-  // const [worksAndPublication, setWorksAndPublication] = useState("");
-  // const [awardAndRecognition, setAwardAndRecognition] = useState("");
+  const [otherFormation, setOtherFormation] = useState("");
+  const [experience, setExperience] = useState("");
+  const [partnership, setPartnership] = useState("");
+  const [worksAndPublication, setWorksAndPublication] = useState("");
+  const [awardAndRecognition, setAwardAndRecognition] = useState("");
 
   const handleChangeFirstname = (e) => {
     setFirstname(e.target.value);
@@ -46,41 +45,41 @@ export default function CreateDoctor() {
   //   setImage(e.target.value);
   // };
 
-  // const handleChangeLanguage = (e) => {
-  //   setLanguage(e.target.value);
-  // };
+  const handleChangeLanguage = (e) => {
+    setLanguage(e.target.value);
+  };
 
-  // const handleChangeBio = (e) => {
-  //   setBio(e.target.value);
-  // };
+  const handleChangeBio = (e) => {
+    setBio(e.target.value);
+  };
 
-  // const handleChangeCertificate = (e) => {
-  //   setCertificate(e.target.value);
-  // };
+  const handleChangeCertificate = (e) => {
+    setCertificate(e.target.value);
+  };
 
   const handleChangeRole = (e) => {
     setRole(e.target.value);
   };
 
-  // const handleChangeOtherFormation = (e) => {
-  //   setOtherFormation(e.target.value);
-  // };
+  const handleChangeOtherFormation = (e) => {
+    setOtherFormation(e.target.value);
+  };
 
-  // const handleChangeExperience = (e) => {
-  //   setExperience(e.target.value);
-  // };
+  const handleChangeExperience = (e) => {
+    setExperience(e.target.value);
+  };
 
-  // const handleChangePartnership = (e) => {
-  //   setPartnership(e.target.value);
-  // };
+  const handleChangePartnership = (e) => {
+    setPartnership(e.target.value);
+  };
 
-  // const handleChangeWorksAndPublication = (e) => {
-  //   setWorksAndPublication(e.target.value);
-  // };
+  const handleChangeWorksAndPublication = (e) => {
+    setWorksAndPublication(e.target.value);
+  };
 
-  // const handleChangeAwardAndRecognition = (e) => {
-  //   setAwardAndRecognition(e.target.value);
-  // };
+  const handleChangeAwardAndRecognition = (e) => {
+    setAwardAndRecognition(e.target.value);
+  };
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -92,15 +91,15 @@ export default function CreateDoctor() {
       mail,
       password,
       // image,
-      // language,
-      // bio,
-      // certificate,
+      language,
+      bio,
+      certificate,
       role,
-      // otherFormation,
-      // experience,
-      // partnership,
-      // worksAndPublication,
-      // awardAndRecognition,
+      otherFormation,
+      experience,
+      partnership,
+      worksAndPublication,
+      awardAndRecognition,
     };
 
     fetch("http://localhost:8000/api/doctors", {
@@ -120,31 +119,24 @@ export default function CreateDoctor() {
     <div className="min-h-screen bg-[#242731]">
       <SideBarDoctor />
       <div className="absolute  w-2/3 mt-[48px] ml-[321px] text-[#FFFFFF]">
-        <HeaderDoctor />
+        <HeaderDoctor text="Un nouveau praticien ?!" />
       </div>
       <div className="flex justify-center items-center h-screen">
-        <div className="absolute left-[321px] top-[172px] rounded-2xl shadow-lg shadow-slate-950/70">
-          <div className="flex mt-[32px]">
-            <div className="flex ml-[35rem] items-center">
-              <button type="button">
-                <img src={edit} alt="edit" className="w-[24px] h-[24px] mr-8" />
-              </button>
-            </div>
-          </div>
+        <div className="absolute  left-[321px] top-[172px] rounded-2xl shadow-lg shadow-slate-950/70 w-[1055px] h-[500px]">
           <section className="relative">
-            <div className="absolute left-24 right-4 top-7 bottom-37 bg-dark-02 rounded-24" />
-            <form onSubmit={handleSubmit} className="p-4">
+            <div className=" bg-dark-02 rounded-24" />
+            <form onSubmit={handleSubmit} className="mt-2 px-4">
               <div className="flex flex-col">
-                <div className="grid grid-cols-2 gap-4">
-                  <div className="flex flex-col items-center">
+                <div className="grid grid-cols-2 gap-4  overflow-y-auto pb-5 h-[420px]">
+                  <div className="flex flex-col items-start">
                     <label
                       htmlFor="firstname"
-                      className="text-2xl mb-2 text-white"
+                      className=" text-base mb-2 text-white"
                     >
                       Prénom
                     </label>
                     <input
-                      className="px-4 py-1 text-black rounded-full"
+                      className="w-96 px-4 py-1 text-black rounded-md"
                       type="text"
                       id="firstname"
                       required
@@ -152,15 +144,15 @@ export default function CreateDoctor() {
                       onChange={handleChangeFirstname}
                     />
                   </div>
-                  <div className="flex flex-col items-center">
+                  <div className="flex flex-col items-start">
                     <label
                       htmlFor="lastname"
-                      className="text-2xl mb-2 text-white"
+                      className="text-base mb-2 text-white"
                     >
                       Nom
                     </label>
                     <input
-                      className="px-4 py-1 text-black rounded-full"
+                      className="w-96 px-4 py-1 text-black rounded-md"
                       type="text"
                       id="lastname"
                       required
@@ -168,65 +160,205 @@ export default function CreateDoctor() {
                       onChange={handleChangeLastname}
                     />
                   </div>
-                  <div className="flex flex-col items-center">
-                    <label htmlFor="tel" className="text-2xl mb-2 text-white">
+                  <div className="flex flex-col items-start">
+                    <label htmlFor="tel" className="text-base mb-2 text-white">
                       Téléphone
                     </label>
                     <input
-                      className="px-4 py-1 text-black rounded-full"
+                      className="w-96 px-4 py-1 text-black rounded-md"
                       type="text"
                       id="tel"
                       value={tel}
                       onChange={handleChangeTel}
                     />
                   </div>
-                  <div className="flex flex-col items-center">
-                    <label htmlFor="mail" className="text-2xl mb-2 text-white">
-                      Mail
-                    </label>
-                    <input
-                      className="px-4 py-1 text-black rounded-full"
-                      type="email"
-                      id="mail"
-                      value={mail}
-                      onChange={handleChangeMail}
-                    />
-                  </div>
-                  <div className="flex flex-col items-center">
-                    <label
-                      htmlFor="password"
-                      className="text-2xl mb-2 text-white"
-                    >
-                      Mot de passe
-                    </label>
-                    <input
-                      className="px-4 py-1 text-black rounded-full"
-                      type="password"
-                      id="password"
-                      value={password}
-                      onChange={handleChangePassword}
-                    />
-                  </div>
-                  <div className="flex flex-col items-center">
-                    <label htmlFor="role" className="text-2xl mb-2 text-white">
+                  <div className="flex flex-col items-start">
+                    <label htmlFor="role" className="text-base mb-2 text-white">
                       Role
                     </label>
                     <input
-                      className="px-4 py-1 text-black rounded-full"
+                      className="w-96 px-4 py-1 text-black rounded-md"
                       type="text"
                       id="role"
                       value={role}
                       onChange={handleChangeRole}
                     />
                   </div>
+
+                  <div className="flex flex-col items-start">
+                    <label htmlFor="mail" className="text-base mb-2 text-white">
+                      Mail
+                    </label>
+                    <input
+                      className="w-96 px-4 py-1 text-black rounded-md"
+                      type="email"
+                      id="mail"
+                      value={mail}
+                      onChange={handleChangeMail}
+                    />
+                  </div>
+                  <div className="flex flex-col items-start">
+                    <label
+                      htmlFor="password"
+                      className="text-base mb-2 text-white"
+                    >
+                      Mot de passe
+                    </label>
+                    <input
+                      className="w-96 px-4 py-1 text-black rounded-md"
+                      type="password"
+                      id="password"
+                      value={password}
+                      onChange={handleChangePassword}
+                    />
+                  </div>
+                  <div className="flex flex-col">
+                    <div className="flex flex-col items-start mb-4">
+                      <label
+                        htmlFor="language"
+                        className="text-base mb-2 text-white"
+                      >
+                        Language
+                      </label>
+                      <textarea
+                        rows={8}
+                        cols={40}
+                        className="w-96 h-20 px-2 py-2 text-black rounded-md"
+                        type="text"
+                        id="language"
+                        value={language}
+                        onChange={handleChangeLanguage}
+                      />
+                    </div>
+
+                    <div className="flex flex-col items-start">
+                      <label
+                        htmlFor="bio"
+                        className="text-base mb-2 text-white"
+                      >
+                        Biographie
+                      </label>
+                      <textarea
+                        rows={8}
+                        cols={40}
+                        className="w-[905px] h-24 px-2 py-2 text-black rounded-md"
+                        type="text"
+                        id="bio"
+                        value={bio}
+                        onChange={handleChangeBio}
+                      />
+                    </div>
+                  </div>
+                  <div className="flex flex-col items-start">
+                    <label
+                      htmlFor="certificate"
+                      className="text-base mb-2 text-white"
+                    >
+                      Diplômes
+                    </label>
+                    <textarea
+                      rows={8}
+                      cols={40}
+                      className="w-96 h-20 px-2 py-2 text-black rounded-md"
+                      type="text"
+                      id="certificate"
+                      value={certificate}
+                      onChange={handleChangeCertificate}
+                    />
+                  </div>
+                  <div className="flex flex-col items-start">
+                    <label
+                      htmlFor="otherFormation"
+                      className="text-base mb-2 text-white"
+                    >
+                      Autres formations
+                    </label>
+                    <textarea
+                      rows={8}
+                      cols={40}
+                      className="w-96 h-20 px-2 py-2 text-black rounded-md"
+                      type="text"
+                      id="otherFormation"
+                      value={otherFormation}
+                      onChange={handleChangeOtherFormation}
+                    />
+                  </div>
+                  <div className="flex flex-col items-start">
+                    <label
+                      htmlFor="experience"
+                      className="text-base mb-2 text-white"
+                    >
+                      Experiences Professionnelles
+                    </label>
+                    <textarea
+                      rows={8}
+                      cols={40}
+                      className="w-96 h-20 px-2 py-2 text-black rounded-md"
+                      type="text"
+                      id="experience"
+                      value={experience}
+                      onChange={handleChangeExperience}
+                    />
+                  </div>
+
+                  <div className="flex flex-col items-start">
+                    <label
+                      htmlFor="worksAndPublication"
+                      className="text-base mb-2 text-white"
+                    >
+                      Travaux et Publications
+                    </label>
+                    <textarea
+                      rows={8}
+                      cols={40}
+                      className="w-96 h-20 px-2 py-2 text-black rounded-md"
+                      type="text"
+                      id="worksAndPublication"
+                      value={worksAndPublication}
+                      onChange={handleChangeWorksAndPublication}
+                    />
+                  </div>
+                  <div className="flex flex-col items-start">
+                    <label
+                      htmlFor="awardAndRecognition"
+                      className="text-base mb-2 text-white"
+                    >
+                      Prix et Reconnaissances
+                    </label>
+                    <textarea
+                      rows={8}
+                      cols={40}
+                      className="w-96 h-20 px-2 py-2 text-black rounded-md"
+                      type="text"
+                      id="awardAndRecognition"
+                      value={awardAndRecognition}
+                      onChange={handleChangeAwardAndRecognition}
+                    />
+                  </div>
+                  <div className="flex flex-col items-start">
+                    <label
+                      htmlFor="partnership"
+                      className="text-base mb-2 text-white"
+                    >
+                      Partenaires Professionnels
+                    </label>
+                    <input
+                      className="w-96 px-4 py-1 text-black rounded-md"
+                      type="text"
+                      id="partnership"
+                      value={partnership}
+                      onChange={handleChangePartnership}
+                    />
+                  </div>
                 </div>
                 {/* Ajoutez d'autres labels ici */}
-                <div className="flex justify-center">
+                <div className="flex justify-center mt-5">
                   <button
                     type="submit"
-                    className="bg-black text-white px-6 py-2 rounded-full mt-6"
+                    className="bg-[#323847] rounded-full shadow-slate-950/90 shadow-xl mb-5 text-white
+                    hover:text-white sm:hover:bg-white/30"
                   >
-                    Enregistrer
+                    <p className="flex px-6 py-2">Enregistrer</p>
                   </button>
                 </div>
               </div>
